df75ba6d040c081d9061c56413833c2b4353ebbf
MatiasCarbel <matiasjcarbel@gmail.com>
2022-04-20 15:05:58 -0300

    commit

--- class02.sql
+++ class02.sql
@@ -5,23 +5,27 @@ USE imdb;
 
 DROP TABLE IF EXISTS film;
 CREATE TABLE film(
-    film_id INT,
+    film_id INT AUTO_INCREMENT,
     description VARCHAR(255),
-    release_year DATE
+    release_year DATE,
+    PRIMARY KEY(film_id)
 );
 
 
 DROP TABLE IF EXISTS actor;
 CREATE TABLE actor(
-    actor_id INT, 
+    actor_id INT AUTO_INCREMENT, 
     first_name VARCHAR(255), 
-    last_name VARCHAR(255)
+    last_name VARCHAR(255),
+    PRIMARY KEY(actor_id)
 );
 
 DROP TABLE IF EXISTS film_actor;
 CREATE TABLE film_actor(
     actor_id INT, 
-    film_id INT
+    film_id INT,
+    FOREIGN KEY (actor_id) REFERENCES actor(actor_id),
+    FOREIGN KEY (film_id) REFERENCES film(film_id)
 );
 
-a
+
