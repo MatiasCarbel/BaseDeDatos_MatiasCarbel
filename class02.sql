337ee7e83ad043e0bfbc94bd39996be5fa650582
MatiasCarbel <matiasjcarbel@gmail.com>
2022-04-20 14:47:15 -0300

    commit

--- class02.sql
+++ class02.sql
@@ -1,1 +1,24 @@
-select *from actor;
+DROP DATABASE IF EXISTS imdb;
+CREATE DATABASE imdb;
+
+USE imdb;
+
+DROP TABLE IF EXISTS film;
+CREATE TABLE film(
+    film_id INT,
+    description VARCHAR(255),
+    release_year DATE
+);
+
+
+DROP TABLE IF EXISTS actor;
+CREATE TABLE actor(
+    actor_id INT, 
+    first_name VARCHAR(255), 
+    last_name VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS film_actor;
+CREATE TABLE film_actor(
+    
+);
