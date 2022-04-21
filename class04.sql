a836d29cb24c57faa06409f5156a761c2aaed613
MatiasCarbel <matiasjcarbel@gmail.com>
2022-04-21 11:27:37 -0300

    1

--- class04.sql
+++ class04.sql
@@ -1,3 +1,8 @@
 USE sakila;
 
 SELECT title, special_features FROM film WHERE rating = 'PG-13';
+
+SELECT DISTINCT length from film;
+
+SELECT title, rental_rate, replacement_cost FROM film WHERE replacement_cost BETWEEN 20.00 AND 24.00;
+
