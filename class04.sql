7d0dc83155c12fb6fc6ac713b701b817bd2743b0
MatiasCarbel <matiasjcarbel@gmail.com>
2022-04-21 11:52:14 -0300

    commit

--- class04.sql
+++ class04.sql
@@ -6,3 +6,4 @@ SELECT DISTINCT length from film;
 
 SELECT title, rental_rate, replacement_cost FROM film WHERE replacement_cost BETWEEN 20.00 AND 24.00;
 
+SELECT f.title, c.name as category, f.rating FROM film as f INNER JOIN film_category as fc ON f.film_id = fc.film_id INNER JOIN category as c ON c.category_id = fc.category_id WHERE;
