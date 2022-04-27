935ae3f274797876f462e95fed55bc7419c09cae
MatiasCarbel <matiasjcarbel@gmail.com>
2022-04-27 10:25:43 -0300

    5

--- class04.sql
+++ class04.sql
@@ -1,9 +1,16 @@
 USE sakila;
 
+#1
 SELECT title, special_features FROM film WHERE rating = 'PG-13';
 
+#2
 SELECT DISTINCT length from film;
 
+#3
 SELECT title, rental_rate, replacement_cost FROM film WHERE replacement_cost BETWEEN 20.00 AND 24.00;
 
+#4
 SELECT f.title, c.name as category, f.rating FROM film as f INNER JOIN film_category as fc ON f.film_id = fc.film_id INNER JOIN category as c ON c.category_id = fc.category_id WHERE f.special_features = 'Behind the Scenes';
+
+#5
+SELECT a.first_name, a.last_name FROM film AS f INNER JOIN film_actor AS fa ON f.film_id = fa.film_id INNER JOIN actor AS a ON fa.actor_id = a.actor_id WHERE f.title = "ZOOLANDER FICTION";
