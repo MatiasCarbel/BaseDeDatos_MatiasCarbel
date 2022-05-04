25b73a63514d44a43c6bbdaaa7a3adece395ea47
MatiasCarbel <matiasjcarbel@gmail.com>
2022-05-04 13:57:29 -0300

    1

--- class06.sql
+++ class06.sql
@@ -1,4 +1,5 @@
 USE sakila;
 
-SELECT DISTINCT (SELECT GROUP_CONCAT(a1.first_name) FROM actor a1 WHERE
-	a1.last_name = a2.last_name) from actor a2;
+SELECT DISTINCT (SELECT GROUP_CONCAT(a1.first_name)FROM actor a1 WHERE
+	a1.last_name = a2.last_name) FROM actor a2;
+A
