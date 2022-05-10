09c51255598c2c1ba725e9c5c993f4e2d1b15331
MatiasCarbel <matiasjcarbel@gmail.com>
2022-05-10 11:37:14 -0300

    1

--- class06.sql
+++ class06.sql
@@ -1,6 +1,16 @@
 USE sakila;
 
-#1 no funciona (preguntar)
-SELECT DISTINCT (SELECT GROUP_CONCAT(a1.first_name)FROM actor a1 WHERE
-	a1.last_name = a2.last_name) FROM actor a2;
+#1 
+SELECT
+	DISTINCT a1.first_name,
+	a1.last_name
+FROM
+	actor a1,
+	actor a2
+WHERE
+	a1.last_name = a2.last_name
+	AND a1.actor_id <> a2.actor_id
+ORDER BY
+	a1.last_name;
 
+#2 
