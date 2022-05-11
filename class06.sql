25b11953a9d7915f24dea0002ea6d8a7f2d1338a
MatiasCarbel <matiasjcarbel@gmail.com>
2022-05-11 11:10:18 -0300

    2

--- class06.sql
+++ class06.sql
@@ -3,13 +3,13 @@ USE sakila;
 #1
 SELECT
     DISTINCT a1.first_name,
-    a1.last_name
+    a1.last_name,
+    a1.actor_id
 FROM
     actor a1,
     actor a2
 WHERE
     a1.last_name = a2.last_name
-    AND a1.actor_id <> a2.actor_id
 ORDER BY
     a1.last_name;
 
@@ -29,3 +29,11 @@ WHERE
     );
 
 #3
+SELECT customer_id FROM customer WHERE customer_id NOT IN (SELECT
+    r1.customer_id
+FROM
+    rental r1,
+    rental r2
+WHERE
+    r1.customer_id = r2.customer_id
+    AND r1.inventory_id <> r2.inventory_id);
