0e009be399547788182f90dbedf6f902ce7e36d2
MatiasCarbel <matiasjcarbel@gmail.com>
2022-05-11 08:48:40 -0300

    2

--- class06.sql
+++ class06.sql
@@ -1,16 +1,31 @@
 USE sakila;
 
-#1 
+#1
 SELECT
-	DISTINCT a1.first_name,
-	a1.last_name
+    DISTINCT a1.first_name,
+    a1.last_name
 FROM
-	actor a1,
-	actor a2
+    actor a1,
+    actor a2
 WHERE
-	a1.last_name = a2.last_name
-	AND a1.actor_id <> a2.actor_id
+    a1.last_name = a2.last_name
+    AND a1.actor_id <> a2.actor_id
 ORDER BY
-	a1.last_name;
+    a1.last_name;
 
-#2 
+#2
+SELECT
+    first_name,
+    last_name,
+    actor_id
+FROM
+    actor
+WHERE
+    EXISTS (
+        SELECT
+            actor_id
+        FROM
+            film_actor
+    );
+
+#3
