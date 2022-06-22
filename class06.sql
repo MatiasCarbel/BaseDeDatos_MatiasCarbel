393b55dfb90613d07e6a9f8f5b6042e3ddad6bfe
MatiasCarbel <matiasjcarbel@gmail.com>
2022-06-22 20:28:13 -0300

    Update class06.sql

--- class06.sql
+++ class06.sql
@@ -112,7 +112,44 @@ WHERE
             )
     );
 
-#Ejercicio de practica
+#6 List the actors that acted in 'BETRAYED REAR' but not in 'CATCH AMISTAD'
+SELECT
+    a.first_name,
+    a.last_name
+FROM
+    actor a
+WHERE
+    a.actor_id IN(
+        SELECT
+            fa.actor_id
+        FROM
+            film_actor fa
+        WHERE
+            fa.film_id IN(
+                SELECT
+                    f.film_id
+                FROM
+                    film f
+                WHERE
+                    title LIKE 'BETRAYED REAR'
+            ) 
+    )AND a.actor_id NOT IN(
+        SELECT
+            fa.actor_id
+        FROM
+            film_actor fa
+        WHERE
+            fa.film_id IN(
+                SELECT
+                    f.film_id
+                FROM
+                    film f
+                WHERE
+                    title LIKE 'CATCH AMISTAD'
+            ) 
+    );
+
+# Ejercicio de practica
 SELECT
     c.customer_id,
     c.first_name,
