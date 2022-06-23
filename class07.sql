fc306fa6608535628e3d68c07dbaf0e6d9b3a961
MatiasCarbel <matiasjcarbel@gmail.com>
2022-06-23 10:36:11 -0300

    2

--- class07.sql
+++ class07.sql
@@ -14,3 +14,6 @@ WHERE
         FROM
             film f2
     );
+
+#2 Write a query that returns the tiltle of the film which duration is the lowest. 
+#If there are more than one film with the lowest durtation, the query returns an empty resultset.
