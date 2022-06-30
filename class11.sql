d6187cd9ad71bbaff25297e595279746847b4112
MatiasCarbel <matiasjcarbel@gmail.com>
2022-06-30 20:54:35 -0300

    6

---
+++ class11.sql
@@ -0,0 +1,41 @@
+-- Active: 1654711325756@@127.0.0.1@3306@sakila
+USE sakila;
+
+#4 Find all the film titles that are not in the inventory.
+SELECT f.title
+FROM film f
+    LEFT JOIN inventory i ON f.film_id = i.film_id
+WHERE i.film_id IS NULL;
+
+#5 Find all the films that are in the inventory but were never rented.
+#Show title and inventory_id.
+#This exercise is complicated.
+#hint: use sub-queries in FROM and in WHERE or use left join and ask if one of the fields is null
+SELECT f.film_id, f.title
+FROM film f
+    INNER JOIN inventory i ON f.film_id = i.film_id
+    LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+WHERE r.inventory_id IS NULL;
+
+#6Generate a report with:
+#customer (first, last) name, store id, film title,
+#when the film was rented and returned for each of these customers
+#order by store_id, customer last_name
+SELECT
+    r.rental_id,
+    r.rental_date,
+    r.return_date,
+    i.film_id,
+    f.title,
+    CONCAT(c.first_name,' ', c.last_name) AS 'Nombre',
+    c.store_id
+FROM rental r
+    INNER JOIN customer c ON r.customer_id = c.customer_id
+    INNER JOIN inventory i ON r.inventory_id = i.inventory_id
+    INNER JOIN film f ON i.film_id = f.film_id
+ORDER BY c.store_id, c.last_name;
+
+#7 Show sales per store (money of rented films)
+#show store's city, country, manager info and total sales (money)
+#(optional) Use concat to show city and country and manager first and last name
+SELECT sto.store_id FROM store sto INNER JOIN staff sta ON sto.manager_staff_id = sta.staff_id ;
