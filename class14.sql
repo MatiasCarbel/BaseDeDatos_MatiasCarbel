43f979e6e5c6a3b5d87dcc84bffa1e81c4fe42a9
MatiasCarbel <matiasjcarbel@gmail.com>
2022-07-08 12:07:34 -0300

    5

--- class14.sql
+++ class14.sql
@@ -48,8 +48,74 @@ FROM film f
  Make sure to "adjust" the input text to try to find the films as effectively as you think is possible.
  */
 
-SELECT ac.actor_id, CONCAT(ac.first_name,' ',ac.last_name),f.film_id, f.title
+SELECT
+    ac.actor_id,
+    CONCAT(
+        ac.first_name,
+        ' ',
+        ac.last_name
+    ),
+    f.film_id,
+    f.title
 FROM film f
     INNER JOIN film_actor USING(film_id)
     INNER JOIN actor ac USING(actor_id)
-    WHERE CONCAT(first_name,' ',last_name) LIKE TRIM(UPPER('PENELOPE GUINESS')) ;
+WHERE
+    CONCAT(first_name, ' ', last_name) LIKE TRIM(UPPER('PENELOPE GUINESS'));
+
+/*4Find all the rentals done in the months of May and June.
+ Show the film title, customer name and if it was returned or not.
+ There should be returned column with two possible values 'Yes' and 'No'.
+ */
+
+SELECT
+    f.title,
+    r.rental_date,
+    c.first_name,
+    CASE
+        WHEN r.return_date IS NOT NULL THEN 'Yes'
+        ELSE 'No'
+    END 'Returned'
+FROM rental r
+    INNER JOIN inventory i USING(inventory_id)
+    INNER JOIN film f USING(film_id)
+    INNER JOIN customer c USING(customer_id)
+WHERE
+    MONTH(r.rental_date) = '05'
+    OR MONTH(r.rental_date) = '06'
+ORDER BY r.rental_date;
+
+/*5Investigate CAST and CONVERT functions.
+ Explain the differences if any, write examples based on sakila DB.
+
+ Cast Function: converts a value of any type into the specified datatype.
+ Syntax: CAST(value AS datatype)
+
+ Convert Fnction: converts a value into the specified datatype or character set.
+ Syntax: CONVERT(value, type) OR CONVERT(value USING charset)
+
+ Differences:
+ Apart from their syntax, Convert and Cast functions have some differences:
+
+-CONVERT can be used to convert data between character sets. Ej: CONVERT(title USING latin1)
+
+-CAST is part of the ANSI-SQL specification(runs in all modern vendor databases).
+ Whereas, CONVERT is not.CONVERT is SQL implementation-specific.
+https://sqlandplsql.com/2011/11/20/what-is-ansi-sql/
+
+
+Types that can be used:
+
+DATE	Converts value to DATE. Format: "YYYY-MM-DD"
+DATETIME	Converts value to DATETIME. Format: "YYYY-MM-DD HH:MM:SS"
+DECIMAL	Converts value to DECIMAL. Use the optional M and D parameters to specify the maximum number of digits (M) and the number of digits following the decimal point (D).
+TIME	Converts value to TIME. Format: "HH:MM:SS"
+CHAR	Converts value to CHAR (a fixed length string)
+NCHAR	Converts value to NCHAR (like CHAR, but produces a string with the national character set)
+SIGNED	Converts value to SIGNED (a signed 64-bit integer)
+UNSIGNED	Converts value to UNSIGNED (an unsigned 64-bit integer)
+BINARY
+
+*/
+
+/*6*/
