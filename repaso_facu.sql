364a451f0c48be2000a610f7f77d0681c8aa68ba
MatiasCarbel <matiasjcarbel@gmail.com>
2024-09-30 17:32:24 -0300

    repaso_facu

---
+++ repaso_facu.sql
@@ -0,0 +1,35 @@
+--Create DB
+CREATE DATABASE [IF NOT EXISTS] database_name;
+
+DROP DATABASE [IF EXISTS] database_name;
+
+
+
+--Use Database
+USE database_name;
+
+
+
+--Create Table
+CREATE TABLE [IF NOT EXISTS] table_name(
+    column1 datatype CONSTRAINT,
+    column2 datatype CONSTRAINT
+)
+
+
+
+--Insert into table
+
+--Update
+
+--Create user and Grant
+
+--Procedures
+
+--Triggers
+
+--Views
+
+--Cursor
+
+--Loop
